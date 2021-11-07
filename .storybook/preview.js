56acf88436a813b26c725d09edd58b46d10c739b
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-07 16:59:20 -0400

    feat: create logo component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,42 +1,21 @@
-import '../.jest/next-image.mock'
-import { addDecorator } from '@storybook/react'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import GlobalStyles from '../src/styles/global'
 import { ThemeProvider } from 'styled-components'
-import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
-import { withNextRouter } from 'storybook-addon-next-router'
-import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
-  backgrounds: {
-    default: 'won-light',
-    values: [
-      {
-        name: 'won-light',
-        value: theme.colors.white
-      },
-      {
-        name: 'won-dark',
-        value: theme.colors.mainBg
-      }
-    ]
+  nextRouter: {
+    Provider: RouterContext.Provider
   }
 }
 
-addDecorator(withNextRouter())
-
 export const decorators = [
-  (Story, context) => (
-    <ThemeProvider theme={theme}>
-      <CartContext.Provider
-        value={{
-          ...CartContextDefaultValues,
-          ...(context?.args?.cartContextValue || {}),
-          ...context.args
-        }}
-      >
-        <GlobalStyles removeBg />
+  (Story) => (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Story />
-      </CartContext.Provider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   )
 ]

f090e76f229cd9f63094db0fb1031e18a203a1c6
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-08 14:18:01 -0400

    feat: create button component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import Button, { ButtonProps } from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      type: ''
+    }
+  }
+} as Meta
+
+export const Default: Story<ButtonProps> = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy now'
+}
+
+export const withIcon: Story<ButtonProps> = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
+}

cff82e4d1c5b2b71eab3cebbb8efc845583b1242
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-11-17 17:03:37 -0400

    feat: create footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,94 @@
+import Link from 'next/link'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact
+        </Heading>
+
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+        <a href="tel:552133283719">+55 21 33283719</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Follow us
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Instagram
+          </a>
+
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Twitter
+          </a>
+
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Youtube
+          </a>
+
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopener, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-labelledby="footer-contact">
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Localization
+        </Heading>
+
+        <span>Rua 7 de Maio</span>
+        <span>527 - 89020330</span>
+        <span>Rio de Janeiro, Brasil</span>
+      </S.Column>
+    </S.Content>
+    <S.Copyright>Won Games 2020 © All rights reserved</S.Copyright>
+  </S.Wrapper>
+)
+
+export default Footer
---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Footer from '.'
+
+describe('<Footer />', () => {
+  it('should render 4 columns topics', () => {
+    renderWithTheme(<Footer />)
+
+    expect(
+      screen.getByRole('heading', { name: /Contact/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Follow us/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /Links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Localization/i })
+    ).toBeInTheDocument()
+  })
+})

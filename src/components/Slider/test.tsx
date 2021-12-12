a430de64ebc02d2ddd1ed71ed3740c1f213aa1a1
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-12 18:16:44 -0400

    feat: create slider component

---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,22 @@
+import 'match-media-mock'
+import { render, screen } from '@testing-library/react'
+
+import Slider from '.'
+
+describe('<Slider />', () => {
+  it('should render children as slider item', () => {
+    render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>One</p> <p>Two</p>
+      </Slider>
+    )
+
+    expect(screen.getByText(/One/i).parentElement?.parentElement).toHaveClass(
+      'slick-slide'
+    )
+
+    expect(screen.getByText(/Two/i).parentElement?.parentElement).toHaveClass(
+      'slick-slide'
+    )
+  })
+})

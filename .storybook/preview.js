6a217fe213bcb7234263bee61a7f114b921a9e6e
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-12 18:19:50 -0400

    ref: add custom background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,20 @@
-import { RouterContext } from 'next/dist/shared/lib/router-context'
 import GlobalStyles from '../src/styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 
 export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
   }
 }
 

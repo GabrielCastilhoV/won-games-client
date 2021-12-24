8a7406dc8a7d8abe31bbe1aa29931745a1dadaac
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-24 18:09:48 -0300

    feat: create text field component

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,57 @@
+import { useState, InputHTMLAttributes } from 'react'
+
+import * as S from './styles'
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void
+  label?: string
+  labelFor?: string
+  initialValue?: string
+  icon?: React.ReactNode
+  iconPosition?: 'left' | 'right'
+  disabled?: boolean
+  error?: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  onInput,
+  label,
+  labelFor = '',
+  initialValue = '',
+  icon,
+  iconPosition = 'left',
+  disabled = false,
+  error,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInput && onInput(newValue)
+  }
+
+  return (
+    <S.Wrapper disabled={disabled} error={!!error}>
+      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
+
+        <S.Input
+          type="text"
+          onChange={onChange}
+          value={value}
+          iconPosition={iconPosition}
+          disabled={disabled}
+          {...props}
+        />
+      </S.InputWrapper>
+
+      {!!error && <S.Error>{error}</S.Error>}
+    </S.Wrapper>
+  )
+}
+
+export default TextField
---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,36 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import TextField, { TextFieldProps } from '.'
+import { Email } from '@styled-icons/material-outlined'
+
+export default {
+  title: 'Form/TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    icon: <Email />,
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+    icon: { type: '' }
+  }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}

903a35a13afa38196e35278330b2b4b461235170
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-25 13:00:51 -0300

    feat: create form sign-in component

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /Sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /Forgot your password/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/Don’t have an account\?/i)).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /Sign up/i })).toBeInTheDocument()
+  })
+})

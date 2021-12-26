0b65f182b0a31112d6b490976081c3f2ecbba86b
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-26 10:23:33 -0300

    refactor: add link in logos

--- src/layouts/Auth/index.tsx
+++ src/layouts/Auth/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import Heading from 'components/Heading'
 import Logo from 'components/Logo'
 import * as S from './styles'
@@ -11,7 +13,11 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo id="banner" />
+        <Link href="/">
+          <a>
+            <Logo id="banner" />
+          </a>
+        </Link>
 
         <div>
           <Heading size="huge">All your favorite games in one place</Heading>
@@ -27,7 +33,11 @@ const Auth = ({ title, children }: AuthProps) => (
 
     <S.Content>
       <S.ContentWrapper>
-        <Logo color="black" size="large" id="form" />
+        <Link href="/">
+          <a>
+            <Logo color="black" size="large" id="form" />
+          </a>
+        </Link>
 
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}

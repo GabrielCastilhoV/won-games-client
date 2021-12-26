99af3bd32c64ddab12ad35e1ba538d42a7c010e3
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-26 10:26:47 -0300

    feat: create page sign-in and sign-up

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,10 @@
+import FormSignIn from 'components/FormSignIn'
+import Auth from 'layouts/Auth'
+
+export default function SignIn() {
+  return (
+    <Auth title="Sign In">
+      <FormSignIn />
+    </Auth>
+  )
+}
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
+import Auth from 'layouts/Auth'
+
+export default function SignUp() {
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  )
+}

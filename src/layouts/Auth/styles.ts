af7be60fa815c712fdf83f078585ee888d636133
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-26 10:24:12 -0300

    style: add fit-content

--- src/layouts/Auth/styles.ts
+++ src/layouts/Auth/styles.ts
@@ -50,6 +50,11 @@ export const BannerContent = styled.div`
     grid-template-columns: 1fr;
     justify-content: space-between;
     height: 100%;
+
+    a {
+      width: fit-content;
+      height: fit-content;
+    }
   `}
 `
 

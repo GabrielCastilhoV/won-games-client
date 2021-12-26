4ced4b2a16eaafd237ade2c34053b9a12b626814
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-26 20:59:39 -0300

    feat: create game detalis component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,38 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import mockGame from './mock'
+
+import GameDetails, { GameDetailsProps } from '.'
+
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame,
+  argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative']
+      }
+    }
+  }
+} as Meta
+
+export const Default: Story<GameDetailsProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: 'auto' }}>
+    <GameDetails {...args} />
+  </div>
+)

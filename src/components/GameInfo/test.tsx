b854032ce09681e71e9a1af8fe7cea7be2f73f15
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-26 21:00:01 -0300

    feat: create game info component

---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameInfo from '.'
+
+const props = {
+  title: 'My game',
+  description: 'My description',
+  price: '200,00'
+}
+
+describe('<GameInfo />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /My game/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/200,00/i)).toBeInTheDocument()
+
+    expect(screen.getByText(/My description/i)).toBeInTheDocument()
+  })
+
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+  })
+})

19bc548ed6a10f87610b8ac8c846b23e2b9b6b49
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-28 12:15:50 -0300

    feat: create gallery component

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,112 @@
+import { useState, useEffect, useRef } from 'react'
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+import SlickSlider from 'react-slick'
+
+import Slider, { SliderSettings } from 'components/Slider'
+
+import * as S from './styles'
+import { Close } from '@styled-icons/material-outlined'
+
+const commonSettings: SliderSettings = {
+  arrows: true,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  nextArrow: <ArrowRight aria-label="next image" />,
+  prevArrow: <ArrowLeft aria-label="previous image" />
+}
+
+const settings: SliderSettings = {
+  ...commonSettings,
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    }
+  ]
+}
+
+const modalSettings: SliderSettings = {
+  ...commonSettings,
+  slidesToShow: 1
+}
+
+export type GalleryImageProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleryImageProps[]
+}
+
+const Gallery = ({ items }: GalleryProps) => {
+  const slider = useRef<SlickSlider>(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [])
+
+  return (
+    <S.Wrapper>
+      <Slider settings={settings} ref={slider}>
+        {items.map((item, index) => (
+          <img
+            key={`thumb-${index}`}
+            role="button"
+            src={item.src}
+            alt={`Thumb -  ${item.label}`}
+            onClick={() => {
+              setIsOpen(true)
+              slider.current!.slickGoTo(index, true)
+            }}
+          />
+        ))}
+      </Slider>
+      <S.Modal isOpen={isOpen} aria-label="modal" aria-hidden={!isOpen}>
+        <S.Close
+          role="button"
+          aria-label="close modal"
+          onClick={() => setIsOpen(false)}
+        >
+          <Close size={40} />
+        </S.Close>
+        <S.Content>
+          <Slider settings={modalSettings} ref={slider}>
+            {items.map((item, index) => (
+              <img key={`gallery-${index}`} src={item.src} alt={item.label} />
+            ))}
+          </Slider>
+        </S.Content>
+      </S.Modal>
+    </S.Wrapper>
+  )
+}
+export default Gallery
---
+++ src/components/Gallery/mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: '/assets/img/games/cyberpunk-1.jpg',
+    label: 'Gallery Image 1'
+  },
+  {
+    src: '/assets/img/games/cyberpunk-2.jpg',
+    label: 'Gallery Image 2'
+  },
+  {
+    src: '/assets/img/games/cyberpunk-3.jpg',
+    label: 'Gallery Image 3'
+  },
+  {
+    src: '/assets/img/games/cyberpunk-4.jpg',
+    label: 'Gallery Image 4'
+  },
+  {
+    src: '/assets/img/games/cyberpunk-5.jpg',
+    label: 'Gallery Image 5'
+  },
+  {
+    src: '/assets/img/games/cyberpunk-6.jpg',
+    label: 'Gallery Image 6'
+  }
+]

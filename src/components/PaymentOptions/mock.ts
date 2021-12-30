1a9b69448874b1999b2203ccb8d6c2495cb6b6c8
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2021-12-30 20:33:16 -0300

    feat: create payment options component

---
+++ src/components/PaymentOptions/mock.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    flag: 'visa',
+    img: '/assets/img/cards/visa.png',
+    number: '*** *** **** 4325'
+  },
+  {
+    flag: 'mastercard',
+    img: '/assets/img/cards/mastercard.png',
+    number: '*** *** **** 4326'
+  }
+]

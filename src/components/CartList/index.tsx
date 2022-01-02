0b3683058a4276bf6190dedca6c8466e9a9320f7
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-01-02 14:26:36 -0300

    feat: create cart-list component

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,20 +1,45 @@
+import Link from 'next/link'
+
+import Button from 'components/Button'
+import Empty from 'components/Empty'
 import GameItem, { GameItemProps } from 'components/GameItem'
+
 import * as S from './styles'
 
 export type CartListProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
+  hasButton?: boolean
 }
 
-const CartList = ({ items, total }: CartListProps) => (
-  <S.Wrapper>
-    {items.map((item) => (
-      <GameItem key={item.title} {...item} />
-    ))}
+const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
+  <S.Wrapper isEmpty={!items.length}>
+    {!!items.length ? (
+      <>
+        {items?.map((item) => (
+          <GameItem key={item.title} {...item} />
+        ))}
 
-    <S.Footer>
-      Total <S.Total>{total}</S.Total>
-    </S.Footer>
+        <S.Footer>
+          {!hasButton && <span>Total:</span>}
+          <S.Total>{total}</S.Total>
+
+          {hasButton && (
+            <Link href="/cart" passHref>
+              <Button as="a">Buy it now</Button>
+            </Link>
+          )}
+        </S.Footer>
+      </>
+    ) : (
+      <>
+        <Empty
+          title="Your cart is empty"
+          description="Go back to the store and explore great games and offers."
+          hasLink
+        />
+      </>
+    )}
   </S.Wrapper>
 )
 
--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -11,4 +11,23 @@ describe('<CartList />', () => {
     expect(screen.getAllByRole('heading')).toHaveLength(2)
     expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' })
   })
+
+  it('should render with button', () => {
+    renderWithTheme(<CartList items={mockItems} total="R$ 330,00" hasButton />)
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
+  })
+
+  it('should render without button', () => {
+    renderWithTheme(<CartList />)
+
+    expect(screen.queryByText(/buy it now/i)).not.toBeInTheDocument()
+  })
+
+  it('should render empty if there are no games', () => {
+    renderWithTheme(<CartList />)
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
+  })
 })

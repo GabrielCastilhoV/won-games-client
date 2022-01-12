4c2cc12f577f96cdd0d4dbdc4cfe2d08f55e5c9f
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-01-11 21:14:07 -0300

    feat: create initialize apollo

---
+++ src/utils/apollo.ts
@@ -0,0 +1,46 @@
+import { useMemo } from 'react'
+
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
+
+import { concatPagination } from '@apollo/client/utilities'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            games: concatPagination()
+          }
+        }
+      }
+    })
+  })
+}
+
+export function initializeApollo(initialState = {}) {
+  const apolloClientGlobal = apolloClient ?? createApolloClient()
+
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState)
+  }
+
+  if (typeof window === 'undefined') return apolloClientGlobal
+  apolloClient = apolloClient ?? apolloClientGlobal
+
+  return apolloClient
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState])
+  return store
+}

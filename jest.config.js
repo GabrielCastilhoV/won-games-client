96eb17ffe6e3f0ac280e91584ae3e49bc175e185
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-01-11 21:06:33 -0300

    refactor: ignore files and rules

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,10 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/**/stories.tsx',
-    '!src/pages/**/*.tsx, !src/styles/**/*.ts'
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/utils/apollo.ts',
+    '!src/type/**/*.d.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']

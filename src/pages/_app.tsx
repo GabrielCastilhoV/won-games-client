bbaf5aa53d3f747d78faeb2df5f8ef26cf42518d
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-01-13 22:12:14 -0300

    feat: add apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import { AppProps } from 'next/app'
 
+import { useApollo } from 'utils/apollo'
+
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from '../styles/theme'
+import { ApolloProvider } from '@apollo/client'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const client = useApollo(pageProps.initialApolloState)
+
   return (
-    <>
+    <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </ApolloProvider>
   )
 }
 

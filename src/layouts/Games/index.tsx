88e273b1a1ea43dd7aa4ff811c9d53ff9a32db9d
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-01-13 22:54:27 -0300

    feat: add use-query-games and implements handle-show-more

--- src/layouts/Games/index.tsx
+++ src/layouts/Games/index.tsx
@@ -1,25 +1,36 @@
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
+import Empty from 'components/Empty'
 
 import Base from 'layouts/Base'
 
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
 import * as S from './styles'
+import { useQueryGames } from 'graphql/queries/games'
 
 export type GamesLayoutProps = {
   games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
-const GamesLayout = ({ filterItems, games = [] }: GamesLayoutProps) => {
+const GamesLayout = ({ filterItems }: GamesLayoutProps) => {
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 }
+  })
+
   const handleFilter = () => {
     return
   }
 
   const handleShowMore = () => {
-    return
+    fetchMore({
+      variables: {
+        limit: 15,
+        start: data?.games.length
+      }
+    })
   }
 
   return (
@@ -27,18 +38,32 @@ const GamesLayout = ({ filterItems, games = [] }: GamesLayoutProps) => {
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
 
-        <section>
-          <Grid>
-            {games.map((game, index) => (
-              <GameCard key={index} {...game} />
-            ))}
-          </Grid>
+        {loading ? (
+          <Empty
+            title="Loading"
+            description="Please wait, the games are loading"
+          />
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover!.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
 
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </S.ShowMore>
-        </section>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <ArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   )
--- src/layouts/Games/test.tsx
+++ src/layouts/Games/test.tsx
@@ -1,9 +1,11 @@
 import { screen } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+
 import { renderWithTheme } from 'utils/tests/helpers'
-import gamesMock from 'components/GameCardSlider/mock'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 
 import Games from '.'
+import { GET_GAMES } from 'graphql/queries/games'
 
 jest.mock('layouts/Base', () => ({
   __esModule: true,
@@ -19,24 +21,61 @@ jest.mock('components/ExploreSidebar', () => ({
   }
 }))
 
-jest.mock('components/GameCard', () => ({
-  __esModule: true,
-  default: function Mock() {
-    return <div data-testid="Mock GameCard" />
-  }
-}))
-
 describe('<Games />', () => {
-  it('should render sections', () => {
+  it('should render loading when starting the template', () => {
     renderWithTheme(
-      <Games filterItems={filterItemsMock} games={[gamesMock[0]]} />
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
     )
 
-    expect(screen.getByTestId('Mock ExploreSidebar')).toBeInTheDocument()
-    expect(screen.getByTestId('Mock GameCard')).toBeInTheDocument()
+    expect(
+      screen.getByText(/Please wait, the games are loading/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should render sections', async () => {
+    renderWithTheme(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: GET_GAMES,
+              variables: { limit: 15 }
+            },
+            result: {
+              data: {
+                games: [
+                  {
+                    name: 'Pathfinder: Wrath of the Righteous - Mythic Edition',
+                    slug: 'pathfinder-wrath-of-the-righteous-mythic-edition',
+                    cover: {
+                      url: '/uploads/pathfinder_wrath_of_the_righteous_mythic_edition_06914c41cc.jpg'
+                    },
+                    developers: [{ name: 'Owlcat Games' }],
+                    price: 252.49,
+                    __typename: 'Game'
+                  }
+                ]
+              }
+            }
+          }
+        ]}
+        addTypename={false}
+      >
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
+    expect(
+      await screen.findByText(
+        /Pathfinder: Wrath of the Righteous - Mythic Edition/i
+      )
+    ).toBeInTheDocument()
 
     expect(
-      screen.getByRole('button', { name: /show more/i })
+      await screen.findByRole('button', { name: /show more/i })
     ).toBeInTheDocument()
   })
 })

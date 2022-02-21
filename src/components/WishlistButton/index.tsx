b431e1f27c7f7d3166017458f1799770b3d92355
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-02-20 23:04:06 -0300

    feat: create component wishlist button

---
+++ src/components/WishlistButton/index.tsx
@@ -0,0 +1,65 @@
+import { useWishlist } from 'hooks/use-wishlist'
+import { useSession } from 'next-auth/client'
+
+import Button, { ButtonProps } from 'components/Button'
+
+import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined'
+import { useState } from 'react'
+import Spinner from 'components/Spinner'
+
+type WishlistButtonProps = {
+  id: string
+  hasText?: boolean
+} & Pick<ButtonProps, 'size'>
+
+const WishlistButton = ({
+  id,
+  hasText,
+  size = 'small'
+}: WishlistButtonProps) => {
+  const [session] = useSession()
+  const [loading, setLoading] = useState(false)
+
+  const {
+    isInWishlist,
+    addToWishlist,
+    removeFromWishlist,
+    loading: loadingApollo
+  } = useWishlist()
+
+  const handleClick = async () => {
+    setLoading(true)
+    isInWishlist(id) ? await removeFromWishlist(id) : await addToWishlist(id)
+
+    setLoading(false)
+  }
+
+  const ButtonText = isInWishlist(id)
+    ? 'Remove from Wishlist'
+    : 'Add to Wishlist'
+
+  if (!session) return null
+
+  return (
+    <Button
+      icon={
+        loading ? (
+          <Spinner />
+        ) : isInWishlist(id) ? (
+          <Favorite aria-label={ButtonText} />
+        ) : (
+          <FavoriteBorder aria-label={ButtonText} />
+        )
+      }
+      onClick={handleClick}
+      minimal
+      size={size}
+      disabled={loadingApollo}
+      style={{ filter: 'none' }}
+    >
+      {hasText && ButtonText}
+    </Button>
+  )
+}
+
+export default WishlistButton
